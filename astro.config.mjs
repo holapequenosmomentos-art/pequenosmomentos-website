5f20344f6ff1cd1e2aca8ad384bfa6fe33fe8422
AxelResendizRzm <ivanramres@gmail.com>
2026-06-18 17:02:41 -0600

    add server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   }
