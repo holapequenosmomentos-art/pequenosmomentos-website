8577c4399bcbbc866b893d41976f5411f103ef50
AxelResendizRzm <ivanramres@gmail.com>
2026-06-19 09:06:40 -0600

    add server to pay

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,13 @@
-import { defineMiddleware } from "astro/middleware";
+// import { defineMiddleware } from "astro/middleware";
 
-export const onRequest = defineMiddleware(async (context, next) => {
-  const host = context.request.headers.get("host");
+// export const onRequest = defineMiddleware(async (context, next) => {
+//   const host = context.request.headers.get("host");
 
-  if (host === "pequenosmomentos.mx") {
-    const url = new URL(context.request.url);
-    url.hostname = "www.pequenosmomentos.mx";
-    return Response.redirect(url.toString(), 301);
-  }
+//   if (host === "pequenosmomentos.mx") {
+//     const url = new URL(context.request.url);
+//     url.hostname = "www.pequenosmomentos.mx";
+//     return Response.redirect(url.toString(), 301);
+//   }
 
-  return next();
-});
+//   return next();
+// });
